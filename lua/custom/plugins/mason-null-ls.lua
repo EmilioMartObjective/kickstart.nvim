c9a4f6efb3bea635d9a04c680d750bbd37dc1550
Emilio <emilio@objectiveinc.com>
2023-05-16 13:02:04 -0600

    feat: autopairs, mason-null-ls, and prisma

---
+++ lua/custom/plugins/mason-null-ls.lua
@@ -0,0 +1,19 @@
+return {
+  "jay-babu/mason-null-ls.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "williamboman/mason.nvim",
+    "jose-elias-alvarez/null-ls.nvim",
+  },
+  config = function()
+    local null_ls = require("null-ls")             -- require your null-ls config here (example below)
+    local formatting = null_ls.builtins.formatting -- to setup formatters
+
+    null_ls.setup({
+      sources = {
+        formatting.prettierd.with({
+          extra_args = { "--single-quote", "--jsx-single-quote"}
+        }) }
+    })
+  end,
+}
